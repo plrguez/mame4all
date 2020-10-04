9bbd8fd1240c22e2e6c555d92e7e53d1c12c00e1
goldmojo <goldmojo@free.fr>
2020-10-04 12:05:54 +0200

    Create a partial OPK with Mame4All UI only

--- distrib/mkopk.sh
+++ distrib/mkopk.sh
@@ -1,2 +1,5 @@
-rm mame4all.opk
+rm -f mame4all.opk
+rm -f mame4all_ui_only.opk
+
 mksquashfs mame4all mame4all.opk
+mksquashfs mame4all mame4all_ui_only.opk -e mame4all_explorer.gcw0.desktop -e aliases.txt
