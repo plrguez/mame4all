3a2fd275cba6a590616fb4ddc61411820edd9659
Pedro Luis Rodríguez González <pl.rguez@gmail.com>
2021-08-05 02:16:10 +0200

    Makefile: Lepus definition and clean dge objects & options for opk file

--- distrib/mkopk.sh
+++ distrib/mkopk.sh
@@ -1,5 +1,5 @@
 rm -f mame4all.opk
 rm -f mame4all_ui_only.opk
 
-mksquashfs mame4all mame4all.opk
-mksquashfs mame4all mame4all_ui_only.opk -e mame4all_explorer.gcw0.desktop -e aliases.txt
+mksquashfs mame4all mame4all.opk -all-root -no-xattrs -noappend -no-exports
+mksquashfs mame4all mame4all_ui_only.opk -e mame4all_explorer.gcw0.desktop -e aliases.txt -all-root -no-xattrs -noappend -no-exports
--- distrib/mkopk_lepus.sh
+++ distrib/mkopk_lepus.sh
@@ -3,7 +3,7 @@ rm -f mame4all_ui_only_lepus.opk
 
 cp ./mame4all/mame4all_explorer.gcw0.desktop ./mame4all/mame4all_explorer.lepus.desktop
 cp ./mame4all/mame4all_ux.gcw0.desktop ./mame4all/mame4all_ux.lepus.desktop
-mksquashfs mame4all mame4all_lepus.opk -e mame4all_explorer.gcw0.desktop -e mame4all_ux.gcw0.desktop
-mksquashfs mame4all mame4all_ui_only_lepus.opk -e mame4all_explorer.gcw0.desktop -e mame4all_ux.gcw0.desktop -e mame4all_explorer.lepus.desktop -e aliases.txt
+mksquashfs mame4all mame4all_lepus.opk -e mame4all_explorer.gcw0.desktop -e mame4all_ux.gcw0.desktop -all-root -no-xattrs -noappend -no-exports
+mksquashfs mame4all mame4all_ui_only_lepus.opk -e mame4all_explorer.gcw0.desktop -e mame4all_ux.gcw0.desktop -e mame4all_explorer.lepus.desktop -e aliases.txt -all-root -no-xattrs -noappend -no-exports
 rm ./mame4all/mame4all_explorer.lepus.desktop
 rm ./mame4all/mame4all_ux.lepus.desktop
